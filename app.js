b007bc3ebef23286368647c73112a8f5abff69e5
CyrilGreco <71821477+CyrilGreco@users.noreply.github.com>
2021-02-11 21:39:06 +0100

    MyMovies

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-require('./models/connection')
+require('dotenv').config();
+require('./models/connection');
 
 var createError = require('http-errors');
 var express = require('express');
@@ -26,19 +27,19 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+	next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
 });
 
 module.exports = app;
--- models/connection.js
+++ models/connection.js
@@ -1,7 +1,11 @@
 //  Dans ce fichier, importez le module mongoose
 var mongoose = require('mongoose');
 
-// initialisation des variables d'environnement pour crypter les données sensibles. 
+// initialisation des variables d'environnement pour crypter les données sensibles.
+// pour les variables d'environnement
+require('dotenv').config();
+var MONGODB_USERNAME = process.env.MONGODB_USERNAME;
+var MONGODB_PASSWORD = process.env.MONGODB_PASSWORD;
 
 var options = {
 	connectTimeoutMS: 5000,
@@ -9,13 +13,16 @@ var options = {
 
 	useUnifiedTopology: true,
 };
-// mongodb+srv://cyril:<password>@cluster0.rp0c6.mongodb.net/<dbname>?retryWrites=true&w=majority
 mongoose.connect(
-	'mongodb+srv://cyril:Azteque69@cluster0.rp0c6.mongodb.net/mymoviezapp?retryWrites=true&w=majority',
+	'mongodb+srv://' +
+		MONGODB_USERNAME +
+		':' +
+		MONGODB_PASSWORD +
+		'@cluster0.rp0c6.mongodb.net/mymoviezapp?retryWrites=true&w=majority',
 	options,
 	function (err) {
 		console.log(err);
 	}
 );
 
-module.exports = mongoose
+module.exports = mongoose;
